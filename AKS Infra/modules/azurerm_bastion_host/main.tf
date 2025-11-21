09e92afa9de1f6f03d1f38b046f74b8eb0734166
shyamubhagat <shyamu.bhagat@gmail.com>
2025-11-21 23:38:33 +0530

    code add

---
+++ AKS Infra/modules/azurerm_bastion_host/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_bastion_host" "bastion" {
+  for_each            = var.bastions
+  name                = each.value.name
+  location            = each.value.location
+  resource_group_name = each.value.resource_group_name
+
+  ip_configuration {
+    name                 = "config"
+    subnet_id            = var.subnet_ids[each.key]
+    public_ip_address_id = var.pip_ids[each.key]
+  }
+}
